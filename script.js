5a3a5b83c930a8dd1b78bc6b28d821a4f2929658
Sreeram P <psreeram3015@proton.me>
2023-11-01 12:32:55 +0530

    Added playRound function for game logic.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function getComputerChoice(){
+function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
     let randomIndex = Math.floor(Math.random() * 3);
     return options[randomIndex]
 }
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice)
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+
+    if (playerChoice == computerChoice) {
+        return "It's a tie!";
+    }
+
+    else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ){
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    }
+
+    else{
+        return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    }
+}
